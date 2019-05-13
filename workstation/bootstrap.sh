64b831c95abd712b0dfa279c53fee9fd5b67702b
Daniel Berg <danberg@us.ibm.com>
2019-05-13 17:04:59 -0400

    update build

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -26,8 +26,8 @@ After=docker.service
 TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
-ExecStartPre=/usr/bin/docker pull danberg/dev:c8239e0
-ExecStart=/usr/bin/docker run -h dev -e TZ=America/New_York --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev danberg/dev:c8239e0
+ExecStartPre=/usr/bin/docker pull danberg/dev:caf557c
+ExecStart=/usr/bin/docker run -h dev -e TZ=America/New_York --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev danberg/dev:caf557c
 
 [Install]
 WantedBy=multi-user.target
