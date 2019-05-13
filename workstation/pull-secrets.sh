b7196e62b8cd418f23237edb9fffd02f5d16d9f4
Daniel Berg <danberg@us.ibm.com>
2019-05-12 23:20:08 -0400

    cleanup

--- workstation/pull-secrets.sh
+++ workstation/pull-secrets.sh
@@ -8,15 +8,18 @@ export OP_SESSION_my=$(op signin https://my.1password.com danberg@us.ibm.com --o
 echo "Pulling secrets"
 # private keys
 op get document 'github_rsa' > github_rsa
+op get document 'jump2_rsa' > jump2_rsa
 #op get document 'zsh_private' > zsh_private
 op get document 'zsh_history' > zsh_history
 
 rm ~/.ssh/github_rsa
+rm ~/.ssh/jump2_rsa
 #rm ~/.zsh_private
 rm ~/.zsh_history
 
 ln -s $(pwd)/github_rsa ~/.ssh/github_rsa
 chmod 0600 ~/.ssh/github_rsa
+ln -s $(pwd)/jump2_rsa ~/.ssh/jump2_rsa
 #ln -s $(pwd)/zsh_private ~/.zsh_private
 ln -s $(pwd)/zsh_history ~/.zsh_history
 
