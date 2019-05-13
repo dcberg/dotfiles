d52103f3ce9553e91553110fbc01a9f5f018b31d
Daniel Berg <danberg@us.ibm.com>
2019-05-13 14:47:35 -0400

    updated entry

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -26,8 +26,8 @@ After=docker.service
 TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
-ExecStartPre=/usr/bin/docker pull danberg/dev:13e9493
-ExecStart=/usr/bin/docker run -h dev -e TZ=America/New_York --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev danberg/dev:13e9493
+ExecStartPre=/usr/bin/docker pull danberg/dev:af86af0
+ExecStart=/usr/bin/docker run -h dev -e TZ=America/New_York --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev danberg/dev:af86af0
 
 [Install]
 WantedBy=multi-user.target
--- workstation/entrypoint.sh
+++ workstation/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ ! -d ~/code/dotfiles ]; then
+#if [ ! -d ~/code/dotfiles ]; then
   echo "Cloning dotfiles"
   # the reason we dont't copy the files individually is, to easily push changes
   # if needed
   cd ~/code
   git clone --recursive https://github.com/dcberg/dotfiles.git
-fi
+#fi
 
 cd ~/code/dotfiles 
 git remote set-url origin git@github.com:dcberg/dotfiles.git
