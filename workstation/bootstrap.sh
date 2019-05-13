5e2007d878647f3442ce799463fb68c7aa6a1887
Daniel Berg <danberg@us.ibm.com>
2019-05-12 22:58:55 -0400

    typo

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -26,7 +26,7 @@ After=docker.service
 TimeoutStartSec=0
 ExecStartPre=-/usr/bin/docker kill dev
 ExecStartPre=-/usr/bin/docker rm dev
-ExecStartPre=/usr/bin/docker pull danberg/dev:lastest
+ExecStartPre=/usr/bin/docker pull danberg/dev:latest
 ExecStart=/usr/bin/docker run -h dev -e TZ=America/New_York --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev danberg/dev:latest
 
 [Install]
