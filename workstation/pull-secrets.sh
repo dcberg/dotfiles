f421b909910ba3625cac2f84091b9ffdf6380b5e
Daniel Berg <danberg@us.ibm.com>
2019-05-15 00:24:21 -0400

    remove secrets

--- workstation/pull-secrets.sh
+++ workstation/pull-secrets.sh
@@ -11,10 +11,10 @@ op get document 'ipadgithub_rsa' > github_rsa
 #op get document 'zsh_private' > zsh_private
 op get document '.zsh_history' > .zsh_history
 
-# rm ~/.ssh/github_rsa
+rm ~/.ssh/github_rsa
 # rm ~/.ssh/jump2_rsa
 #rm ~/.zsh_private
-# rm ~/.zsh_history
+rm ~/.zsh_history
 
 ln -s $(pwd)/github_rsa ~/.ssh/github_rsa
 chmod 0600 ~/.ssh/github_rsa
